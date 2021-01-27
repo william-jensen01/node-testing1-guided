c0c93714a92f6198d308b2fe756dc5f7f970ae7c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-27 18:24:24 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,4 +1,6 @@
 // Test away!
 it('works', () => {
-  expect(3).toBe(4)
+  expect(3).toBe(3) // eslint-disable-line
+  expect(3).nottoBe(3) // eslint-disable-line
+  expect(3).toBe(3) // eslint-disable-line
 })
